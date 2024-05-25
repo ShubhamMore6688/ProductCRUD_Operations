f513b4b3beeabd7479cef48b01cf22ed9b919947
Shubham More <shubhamdmore03@gmail.com>
2024-05-25 11:11:02 +0530

    operation for add and delete product

---
+++ src/main/com/java/product/controller/ProductController.java
@@ -0,0 +1,56 @@
+package com.java.product.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+
+import com.java.product.doa.ProductDoa;
+import com.java.product.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    private ProductDoa productDoa;
+
+    @RequestMapping("/")
+    public String home(Model m){
+        List<Product> products = productDoa.getAllProducts();
+        m.addAttribute("products", products);
+        return "home";
+    }
+
+    @RequestMapping("/product")
+    public String handle(){
+
+        return "product";
+    }
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    public RedirectView addProduct(@ModelAttribute Product product, HttpServletRequest request){
+        this.productDoa.createProduct(product);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(request.getContextPath() + "/");
+        return redirectView;
+    }
+
+    @RequestMapping(path = "/delete/{productId}")
+    public RedirectView deleteProduct( @PathVariable("productId") int prouductId,  HttpServletRequest request){
+        this.productDoa.deleteProduct(prouductId);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(request.getContextPath() + "/");
+
+        return redirectView;
+    }
+
+    
+}
---
+++ src/main/com/java/product/doa/ProductDoa.java
@@ -0,0 +1,45 @@
+package com.java.product.doa;
+
+import com.java.product.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Component
+public class ProductDoa {
+
+    @Autowired
+    private HibernateTemplate hibernateTemplate;
+
+    @Transactional
+    public void createProduct(Product product){
+        this.hibernateTemplate.save(product);
+    }
+
+    public List<Product> getAllProducts(){
+        List<Product> products = this.hibernateTemplate.loadAll(Product.class);
+        return products;
+    }
+
+    public Product getProduct(int productid){
+        Product product = this.hibernateTemplate.get(Product.class, productid);
+        return product;
+    }
+
+    @Transactional
+    public void updateProduct(Product product){
+        this.hibernateTemplate.update(product);
+    }
+
+    @Transactional
+    public void deleteProduct(int productid){
+        Product product = this.hibernateTemplate.get(Product.class, productid);
+        this.hibernateTemplate.delete(product);
+    }
+
+
+
+}
